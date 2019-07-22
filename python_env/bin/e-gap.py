5487ce4d13bef821e16f701ca4e02a66e14ffca7
RexGalilae <mdzaidgr8@gmail.com>
2019-07-22 21:10:28 +0400

    tox finally works again

---
+++ python_env/bin/e-gap.py
@@ -0,0 +1,7 @@
+#!/home/alhazen/Desktop/Coding/VASP-script-package/vasp-git/vasp_tools/python_env/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'vasp-tools==0.1.0','e-gap.py'
+__requires__ = 'vasp-tools==0.1.0'
+__import__('pkg_resources').require('vasp-tools==0.1.0')
+__file__ = '/home/alhazen/Desktop/Coding/VASP-script-package/vasp-git/vasp_tools/scripts/e-gap.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
---
+++ python_env/bin/split-by.py
@@ -0,0 +1,7 @@
+#!/home/alhazen/Desktop/Coding/VASP-script-package/vasp-git/vasp_tools/python_env/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'vasp-tools==0.1.0','split-by.py'
+__requires__ = 'vasp-tools==0.1.0'
+__import__('pkg_resources').require('vasp-tools==0.1.0')
+__file__ = '/home/alhazen/Desktop/Coding/VASP-script-package/vasp-git/vasp_tools/scripts/split-by.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
