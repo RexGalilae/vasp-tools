fe8091a15ec2c8711d3c4b4c5eed8a69070fd9d1
RexGalilae <mdzaidgr8@gmail.com>
2019-07-22 15:33:54 +0400

    Update setup to make package pip-ready
    pip install -e . works on curr env

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
     # TODO: put package requirements here
 ]
 
@@ -23,11 +22,20 @@ setup(
     version='0.1.0',
     description="A suite of scripts that perform menial, tedious and repetitive tasks and operations on VASP files that were hurriedly and haphazardly put together by some intern.",
     long_description=readme + '\n\n' + history,
-    author="Zaid Hassan",
+    author="Zaid Hassan aka RexGalilae",
     author_email='mdzaidgr8@gmail.com',
     url='https://github.com/RexGalilae/vasp_tools',
     packages=[
         'vasp_tools',
+    ],
+    scripts = [
+        'scripts/cart-direct.py',
+        'scripts/e-gap.py',
+        'scripts/e-plot.py',
+        'scripts/fix-upto.py',
+        'scripts/place-at.py',
+        'scripts/rotate.py',
+        'scripts/split-by.py',
     ],
     package_dir={'vasp_tools':
                  'vasp_tools'},
@@ -40,7 +48,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='vasp_tools',
+    keywords='vasp python computation dft',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -53,6 +61,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements
